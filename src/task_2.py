8b22b3c7429e904cb29e2db7dc533a2bec4d26d8
Alejandro Viegener <alejandro.viegener@globallogic.com>
2024-02-28 14:40:39 -0300

    added parameters to the job

--- src/task_2.py
+++ src/task_2.py
@@ -1,4 +1,18 @@
 
 # Dummy task
+import argparse
 
-print("Hello from task 2")
+def main():
+    parser = argparse.ArgumentParser(description="Process some parameters.")
+    parser.add_argument('--param1', type=str, help='The first parameter')
+    parser.add_argument('--param2', type=str, help='The second parameter')
+
+    args = parser.parse_args()
+
+    # Now you can use args.param1 and args.param2 in your script
+    print("Hello from task 2")
+    print(f"Parameter 1: {args.param1}")
+    print(f"Parameter 2: {args.param2}")
+
+if __name__ == "__main__":
+    main()
--- src/task_3.py
+++ src/task_3.py
@@ -1,4 +1,17 @@
 # Dummy task
+import argparse
 
+def main():
+    parser = argparse.ArgumentParser(description="Process some parameters.")
+    parser.add_argument('--param1', type=str, help='The first parameter')
+    parser.add_argument('--param2', type=str, help='The second parameter')
 
-print("Hello from task 3")
+    args = parser.parse_args()
+
+    # Now you can use args.param1 and args.param2 in your script
+    print("Hello from task 3")
+    print(f"Parameter 1: {args.param1}")
+    print(f"Parameter 2: {args.param2}")
+
+if __name__ == "__main__":
+    main()
